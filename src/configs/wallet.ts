173f6e9e77b41a23e5dde912fadb2bd2e480ce6a
xiaomabenteng2015 <xiaomabenteng@look.com>
2024-09-13 10:57:06 +0800

    配置projectId

--- src/configs/wallet.ts
+++ src/configs/wallet.ts
@@ -3,16 +3,17 @@ import { mainnet, arbitrum, optimism, immutableZkEvm } from 'wagmi/chains'
 import { walletConnect, injected } from 'wagmi/connectors'
 
 // a temp project id for testing
-const projectId = 'd20062f0065b7c85019c0c995a61f910'
+const projectId = '4703d1a7a30b63665f8d8e8339a9aceb'
 
-export const WCConnector = walletConnect({ 
+export const WCConnector = walletConnect({
   projectId,
   metadata: {
     icons: ['https://example.com/icon.png'],
-    name: 'Example', 
-    description: 'Example website', 
-    url: 'https://example.com', 
-  }, })
+    name: 'Example',
+    description: 'Example website',
+    url: 'https://example.com',
+  },
+})
 
 export const config = createConfig({
   chains: [mainnet, optimism, arbitrum, immutableZkEvm],
